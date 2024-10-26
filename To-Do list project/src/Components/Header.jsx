3d829fc12a9a9e31811ca393d46d8f05c171d281
devvishnoi <devvishnoi.6@gmail.com>
2024-10-26 20:03:38 +0530

    commit

--- To-Do list project/src/Components/Header.jsx
+++ To-Do list project/src/Components/Header.jsx
@@ -7,12 +7,13 @@ function Header({inputValue , setinputValue , addList}){
     return(
         <>
         <div className="header">
+            <a href="https://github.com/Devvishnoi6/To-Do-vite-react-assignment" target= "_blank" >🔗 GITHUB</a>
             <h1>To-Do List</h1>
             <input
              type="text"
              value={inputValue}
              onChange={(e) => setinputValue(e.target.value)}
-             placeholder="Add Something to your To-Do List" />
+             placeholder="Add Something to your To-Do List 🙂" />
              <button onClick={addList}>Add</button>
         </div>
         </>
